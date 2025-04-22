877e397b203c71460f40c46d75b6591792ccbea6
finnafinest_ <jitupersonalacc@gmail.com>
2025-04-22 10:28:29 -0700

    MiscUtils class

--- misc-utilities.js
+++ misc-utilities.js
@@ -1,1 +1,15 @@
+export class MiscUtils {
 
+  /**
+    * @author frostice482
+    * 
+    * Renames function name
+    * @param fn Function
+    * @param name New function name
+    * @returns function
+    */
+  static renameFn(fn, name) {
+	  return Object.defineProperty(fn, "name", { value: name })
+  }
+  
+}
