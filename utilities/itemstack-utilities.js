fb7f15a9a5723029942d3564687c63c9b89e59db
finnafinest_ <jitupersonalacc@gmail.com>
2025-04-25 02:53:36 -0700

    Update itemstack-utilities.js

--- utilities/itemstack-utilities.js
+++ utilities/itemstack-utilities.js
@@ -3,20 +3,45 @@ import { world, BlockPermutation, ItemStack, EquipmentSlot, system } from "@mine
 export class ItemStackUtils {
 
     /**
-     * @author GegaMC
-     * @remarks Check if ItemStack is a block. For example, a minecraft:stone is a block, but minecraft:iron_shovel isn't.
-     * @param {ItemStack} itemStack
-     * @returns {Boolean}
-     * @example
-     * import { world } from "@minecraft/server";
-     * 
-     * world.afterEvents.itemUse.subcscribe((event)=>{
-     *     const { source, itemStack } = event;
-     *     if (isBlock(itemStack)) {
-     *         source.sendMessage("Im a placeable block!")
-     *     }
-     * })
-     */
+      * Transfer enchantments from an item to another
+      * 
+      * @param {ItemStack} sourceItem Item to grab enchantments from
+      * @param {ItemStack} destinationItem Item to transfer enchantments to
+      * @returns {ItemStack}
+      * 
+      * @example
+      * import { world } from "@minecraft/server"
+      * 
+      * const player = world.getPlayers()[0];
+      * const sourceItem = player.getComponent("inventory").container.getItem(0);
+      * const destinationItem = player.getComponent("inventory").container.getItem(1);
+      * const transferedEnchants = transferEnchantments(sourceItem, destinationItem);
+      * player.getComponent("inventory").container.setItem(1, transferedEnchants);
+      * 
+      * @throws If sourceItem is not enchantable
+      * @throws If destinationItem is not enchantable
+      */
+    static transferEnchantments(sourceItem, destinationItem) {
+        const sourceEnchantable = sourceItem.getComponent("enchantable");
+        if (!sourceEnchantable)
+            throw new Error("Source item is not enchantable");
+        const destinationEnchantable = destinationItem.getComponent("enchantable");
+        if (!destinationEnchantable)
+            throw new Error("Destination item is not enchantable");
+        for (const enchantment of sourceEnchantable.getEnchantments()) {
+            if (!destinationEnchantable.canAddEnchantment(enchantment))
+                continue;
+            destinationEnchantable.addEnchantment(enchantment);
+        }
+        return destinationItem;
+    }
+
+    /**
+      * @author GegaMC
+      * @description Determine if ItemStack is a type of block
+      * @param {ItemStack} itemStack
+      * @returns {Boolean}
+      */
     static isBlock(itemStack) {
         try {
             BlockPermutation.resolve(itemStack.typeId)
@@ -27,18 +52,9 @@ export class ItemStackUtils {
 
     /**
      * @author GegaMC
-     * @remarks Retrieve the EquipmentSlot that this ItemStack can be worn into. For example, an iron chestplate will return "Chest", a totem of undying return "Offhand", whilst a diamond sword which can't be worn or offhanded will defaults to "Mainhand"
+     * @description Get an EquipmentSlot that this ItemStack can be put into.
      * @param {ItemStack} itemStack
      * @returns {EquipmentSlot}
-     * @example
-     * import { world } from "@minecraft/server";
-     * 
-     * world.afterEvents.itemUse.subcscribe((event)=>{
-     *     const { source, itemStack } = event;
-     *     const equipmentSlot = getWearableSlot(itemStack);
-     *     
-     *     source.sendMessage(`This item is worn on the ${equipmentSlot} Slot`)
-     * })
      */
     static getWearableSlot(itemStack) {
         //sacrifice random player >:D
