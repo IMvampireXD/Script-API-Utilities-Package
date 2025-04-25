78c99ca35cb95eddae369c89046758aac1890340
finnafinest_ <jitupersonalacc@gmail.com>
2025-04-25 02:14:03 -0700

    Moved functions to class

--- utilities/itemstack-utilities.js
+++ utilities/itemstack-utilities.js
@@ -1,50 +1,54 @@
 import { world, BlockPermutation, ItemStack, EquipmentSlot, system } from "@minecraft/server";
 
-/**
- * @author GegaMC
- * @description Determine if ItemStack is a type of block
- * @param {ItemStack} itemStack
- * @returns {Boolean}
- */
-export function isBlock(itemStack) {
-    try {
-        BlockPermutation.resolve(itemStack.typeId)
-        return true;
-    } catch {}
-    return false
-}
+export class ItemStackUtils {
 
-/**
- * @author GegaMC
- * @description Get an EquipmentSlot that this ItemStack can be put into.
- * @param {ItemStack} itemStack
- * @returns {EquipmentSlot}
- */
-function getWearableSlot(itemStack) {
-    //sacrifice random player >:D
-    const rPlayer = world.getPlayers()[0];
-    const equippable = rPlayer.getComponent("equippable");
-    const slots = ["Head", "Chest", "Legs", "Feet","Offhand","Mainhand"];
-    const prevItemStack = slots.map(s=>equippable.getEquipment(s));
-    let slot;
-    for (const s of slots)
-    if (equippable.setEquipment(s,itemStack)) {
-        slot = s;
-        break;
+    /**
+      * @author GegaMC
+      * @description Determine if ItemStack is a type of block
+      * @param {ItemStack} itemStack
+      * @returns {Boolean}
+      */
+    static isBlock(itemStack) {
+        try {
+            BlockPermutation.resolve(itemStack.typeId)
+            return true;
+        } catch {}
+        return false
     }
-    prevItemStack.forEach((e,i)=>{
-        equippable.setEquipment(slots[i],e)
-    })
-    system.waitTicks(2).then(()=>{
-        [
-            "armor.equip_chain",
-            "armor.equip_diamond",
-            "armor.equip_generic",
-            "armor.equip_gold",
-            "armor.equip_iron",
-            "armor.equip_leather",
-            "armor.equip_netherite"
-        ].forEach(sound=>rPlayer.runCommand(`stopsound @s ${sound}`))
-    })
-    return slot
+
+    /**
+     * @author GegaMC
+     * @description Get an EquipmentSlot that this ItemStack can be put into.
+     * @param {ItemStack} itemStack
+     * @returns {EquipmentSlot}
+     */
+    static getWearableSlot(itemStack) {
+        //sacrifice random player >:D
+        const rPlayer = world.getPlayers()[0];
+        const equippable = rPlayer.getComponent("equippable");
+        const slots = ["Head", "Chest", "Legs", "Feet","Offhand","Mainhand"];
+        const prevItemStack = slots.map(s=>equippable.getEquipment(s));
+        let slot;
+        for (const s of slots)
+        if (equippable.setEquipment(s,itemStack)) {
+            slot = s;
+            break;
+        }
+        prevItemStack.forEach((e,i)=>{
+            equippable.setEquipment(slots[i],e)
+        })
+        system.waitTicks(2).then(()=>{
+            [
+                "armor.equip_chain",
+                "armor.equip_diamond",
+                "armor.equip_generic",
+                "armor.equip_gold",
+                "armor.equip_iron",
+                "armor.equip_leather",
+                "armor.equip_netherite"
+            ].forEach(sound=>rPlayer.runCommand(`stopsound @s ${sound}`))
+        })
+        return slot
+    }
+
 }
